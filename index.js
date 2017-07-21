2e459846f1710daf7aa212128e0e4d0ebca3af3b
sanjoy sardar <sksardar42@gmail.com>
2017-07-21 20:55:09 +0530

    added all files

---
+++ index.js
@@ -0,0 +1,19 @@
+var expect = require('expect');
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+expect(counter(undefined, { type: 'INCREMENT' })).toEqual(1);
+expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
+expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
+expect(counter(1, { type: 'DECREMENT' })).toEqual(0);
+expect(counter(0, { type: 'SOME_THING' })).toEqual(0);
+// // expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+console.log('tests passed!');
