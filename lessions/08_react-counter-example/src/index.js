1eeb8cc18c58bf3cc2f0c6b2864d88113048c17b
sanjoy sardar <sksardar42@gmail.com>
2017-07-22 22:03:17 +0530

    added react counter example

--- lessions/08_react-counter-example/src/index.js
+++ lessions/08_react-counter-example/src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+const store = createStore(counter);
+const Counter = ({ value, onIncrement, onDecrement }) =>
+    <div>
+        <h1>
+            {value}
+        </h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>;
+const render = () => {
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+            onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+        />,
+        document.getElementById('root')
+    );
+};
+store.subscribe(render);
+render();
+
 registerServiceWorker();
