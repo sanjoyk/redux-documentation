31f42939d0918e177bdf6beb8a32bbaec8a43a44
sanjoy sardar <sksardar42@gmail.com>
2017-07-23 18:01:02 +0530

    used combind reeducers

--- lessions/15_reducer-composition-with-combinned-reducer/src/index.js
+++ lessions/15_reducer-composition-with-combinned-reducer/src/index.js
@@ -1,8 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const todo = (state = {}, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if (action.id !== state.id) {
+                return state;
+            }
+            return {
+                ...state,
+                completed: !state.completed
+            };
+        default:
+            return state;
+    }
+};
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+    switch (action.type) {
+        case 'SHOW_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, todo(undefined, action)];
+        case 'TOGGLE_TODO':
+            return state.map(t => todo(t, action));
+        default:
+            return state;
+    }
+};
+
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+});
+const store = createStore(todoApp);
+console.log(store.getState());
+
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn React'
+});
+console.log(store.getState());
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 1,
+    text: 'Learn Redux'
+});
+console.log(store.getState());
+store.dispatch({
+    type: 'TOGGLE_TODO',
+    id: 1
+});
+console.log(store.getState());
+store.dispatch({
+    type: 'SHOW_VISIBILITY_FILTER',
+    filter: 'SHOW_ACTIVE'
+});
+console.log(store.getState());
+
+console.log('all test passed');
 registerServiceWorker();
