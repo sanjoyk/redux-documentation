c8a8fecef9cddd1d2ca44b2d2ee41210a9dd6287
sanjoy sardar <sksardar42@gmail.com>
2017-07-23 16:43:27 +0530

    reducer composition with object is shown in 14 and 15 is created

--- lessions/14_reducer-composition-with-object/src/index.js
+++ lessions/14_reducer-composition-with-object/src/index.js
@@ -1,8 +1,125 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const todo = (state = {}, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if (action.id !== state.id) {
+                return state;
+            }
+            return {
+                ...state,
+                completed: !state.completed
+            };
+        default:
+            return state;
+    }
+};
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+    switch (action.type) {
+        case 'SHOW_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+const todoApp = (state = {}, action) => {
+    return {
+        todos: todos(state.todos, action),
+        visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+    };
+};
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, todo(undefined, action)];
+        case 'TOGGLE_TODO':
+            return state.map(t => todo(t, action));
+        default:
+            return state;
+    }
+};
+const store = createStore(todoApp);
+console.log(store.getState());
+
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn React'
+});
+console.log(store.getState());
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 1,
+    text: 'Learn Redux'
+});
+console.log(store.getState());
+store.dispatch({
+    type: 'TOGGLE_TODO',
+    id: 1
+});
+console.log(store.getState());
+
+//
+// const testAddTodo = () => {
+//     const stateBefore = [];
+//     const action = {
+//         type: 'ADD_TODO',
+//         id: 0,
+//         text: 'Learn Redux'
+//     };
+//     const stateAfter = [
+//         {
+//             id: 0,
+//             text: 'Learn Redux',
+//             completed: false
+//         }
+//     ];
+//     deepFreeze(stateBefore);
+//     deepFreeze(action);
+//     expect(todos(stateBefore, action)).toEqual(stateAfter);
+// };
+// const testToggleTodo = () => {
+//     const stateBefore = [
+//         {
+//             id: 0,
+//             text: 'learn Redux',
+//             completed: false
+//         },
+//         {
+//             id: 1,
+//             text: 'learn React',
+//             completed: false
+//         }
+//     ];
+//     const stateAfter = [
+//         {
+//             id: 0,
+//             text: 'learn Redux',
+//             completed: false
+//         },
+//         {
+//             id: 1,
+//             text: 'learn React',
+//             completed: true
+//         }
+//     ];
+//     const action = {
+//         type: 'TOGGLE_TODO',
+//         id: 1
+//     };
+//     deepFreeze(stateBefore);
+//     deepFreeze(action);
+//     expect(todos(stateBefore, action)).toEqual(stateAfter);
+// };
+// testAddTodo();
+// testToggleTodo();
+
+console.log('all test passed');
 registerServiceWorker();
