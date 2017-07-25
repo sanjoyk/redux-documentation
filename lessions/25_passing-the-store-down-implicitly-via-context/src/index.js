5fcb0bd9bfed3b11848f60f077ac9d2ce839574c
sanjoy sardar <sksardar42@gmail.com>
2017-07-25 18:10:20 +0530

    pass store as context using Provider class

---
+++ lessions/25_passing-the-store-down-implicitly-via-context/src/index.js
@@ -0,0 +1,237 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
+
+const todo = (state = {}, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if (action.id !== state.id) {
+                return state;
+            }
+            // state.completed = !state.completed;
+            // return state;
+            return {
+                ...state,
+                completed: !state.completed
+            };
+        default:
+            return state;
+    }
+};
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+    switch (action.type) {
+        case 'SHOW_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, todo(undefined, action)];
+        case 'TOGGLE_TODO':
+            return state.map(t => todo(t, action));
+        default:
+            return state;
+    }
+};
+
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+});
+
+const Link = ({ active, children, onClick }) => {
+    if (active) {
+        return (
+            <span>
+                {children}
+            </span>
+        );
+    }
+    return (
+        <a
+            href="#"
+            onClick={e => {
+                e.preventDefault();
+                onClick();
+            }}
+        >
+            {children}
+        </a>
+    );
+};
+class FilterLink extends Component {
+    componentDidMount() {
+        const { store } = this.context;
+        this.unsubscribe = store.subscribe(() => {
+            this.forceUpdate();
+        });
+    }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+    render() {
+        const props = this.props;
+        const { store } = this.context;
+        const state = store.getState();
+        return (
+            <Link
+                active={props.filter === state.visibilityFilter}
+                onClick={() => {
+                    store.dispatch({
+                        type: 'SHOW_VISIBILITY_FILTER',
+                        filter: props.filter
+                    });
+                }}
+            >
+                {props.children}
+            </Link>
+        );
+    }
+}
+FilterLink.contextTypes = {
+    store: React.PropTypes.object
+};
+const Footer = () => {
+    return (
+        <p>
+            Show : <FilterLink filter="SHOW_ALL">All</FilterLink>{' '}
+            <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>{' '}
+            <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>
+        </p>
+    );
+};
+Footer.contextTypes = {
+    store: React.PropTypes.object
+};
+const getVisibilityTodos = (todos, visibilityFilter) => {
+    switch (visibilityFilter) {
+        case 'SHOW_ALL':
+            return todos;
+        case 'SHOW_ACTIVE':
+            return todos.filter(todo => !todo.completed);
+        case 'SHOW_COMPLETED':
+            return todos.filter(todo => todo.completed);
+        default:
+            return todos;
+    }
+};
+
+const Todo = ({ text, onClick, completed }) => {
+    return (
+        <li
+            onClick={onClick}
+            style={{
+                textDecoration: completed ? 'line-through' : 'none'
+            }}
+        >
+            {text}
+        </li>
+    );
+};
+const TodoList = ({ todos, onTodoClick }) => {
+    return (
+        <ul>
+            {todos.map(todo =>
+                <Todo
+                    {...todo}
+                    key={todo.id}
+                    onClick={() => onTodoClick(todo.id)}
+                />
+            )}
+        </ul>
+    );
+};
+
+class VisibleTodoList extends Component {
+    componentDidMount() {
+        const { store } = this.context;
+        this.unsubscribe = store.subscribe(() => {
+            this.forceUpdate();
+        });
+    }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+    render() {
+        const { store } = this.context;
+        const { todos, visibilityFilter } = store.getState();
+        return (
+            <TodoList
+                todos={getVisibilityTodos(todos, visibilityFilter)}
+                onTodoClick={id =>
+                    store.dispatch({
+                        type: 'TOGGLE_TODO',
+                        id
+                    })}
+            />
+        );
+    }
+}
+VisibleTodoList.contextTypes = {
+    store: React.PropTypes.object
+};
+const AddTodo = (props, { store }) => {
+    let input;
+    return (
+        <div>
+            <input type="text" ref={node => (input = node)} />
+            <button
+                onClick={() => {
+                    store.dispatch({
+                        type: 'ADD_TODO',
+                        id: nextTodoId++,
+                        text: input.value
+                    });
+                    input.value = '';
+                }}
+            >
+                add todo
+            </button>
+        </div>
+    );
+};
+AddTodo.contextTypes = {
+    store: React.PropTypes.object
+};
+let nextTodoId = 0;
+const TodoApp = () => {
+    return (
+        <div>
+            <AddTodo />
+            <VisibleTodoList />
+            <Footer />
+        </div>
+    );
+};
+class Provider extends Component {
+    getChildContext() {
+        return {
+            store: this.props.store
+        };
+    }
+    render() {
+        return this.props.children;
+    }
+}
+Provider.childContextTypes = {
+    store: React.PropTypes.object
+};
+ReactDOM.render(
+    <Provider store={createStore(todoApp)}>
+        <TodoApp />
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
